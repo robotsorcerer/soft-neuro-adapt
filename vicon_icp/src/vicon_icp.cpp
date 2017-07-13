c200288eed3b04bb82a6690db98435567fb623a5
lakehanne <patlekano@gmail.com>
2017-07-13 17:24:16 -0500

    working but not stopping after count 3

--- vicon_icp/src/vicon_icp.cpp
+++ vicon_icp/src/vicon_icp.cpp
@@ -1,8 +1,8 @@
 /*
-        Olalekan Ogunmolu. 
+        Olalekan Ogunmolu.
         June 01, 2017
 
-        See: A Model for Registration of 3D shapes, 
+        See: A Model for Registration of 3D shapes,
              Paul Besl and Neil D. McKay
 
              Eqs 23 - 27
@@ -28,29 +28,25 @@
 using namespace Eigen;
 
 class Receiver
-{ 
+{
 private:
     int count, num_points; //iterator and number of markers on object
-
-    double mu_p, mu_x;
-
-    std::vector<geometry_msgs::Point> headMarkersVector, 
+    std::vector<geometry_msgs::Point> headMarkersVector,
                                     firstHeadMarkersVector;
     std::vector<Vector3d> face_vec, first_face_vec;
-    //covariance matrix
-    Eigen::Matrix3d sigma_px, temp;   
-    //identity matrix involved in Q matrix
-    Eigen::Matrix3d I3;                               
+    // covariance matrix
+    Matrix3d sigma_px, temp;
+    // identity matrix involved in Q matrix
+    Matrix3d I3;
 
-    Eigen::Matrix3d A_Mat;
+    Matrix3d A_Mat;
 
-    //Form Q from which we compute the rotation quaternion
-    Eigen::Matrix4d Q;
-    //Delta
+    // Form Q from which we compute the rotation quaternion
+    Matrix4d Q;
+    // Delta
     Vector3d Delta;
-    //will contain rotationand translation of the head
+    // will contain rotationand translation of the head
     geometry_msgs::Transform pose_info;
-
     geometry_msgs::Point face_translation;
 
     ros::NodeHandle nm_;
@@ -59,23 +55,25 @@ private:
     ros::AsyncSpinner spinner;
     unsigned long const hardware_threads;
 
-    //pose vector
+    // pose vector
     ros::Subscriber sub_markers;
 
-    std::thread rotoTransThread;          
+    std::thread rotoTransThread;
     double roll, pitch, yaw;
+    double mu_p, mu_x;
     double x, y, z;
+    double q1, q2, q3, q4;
 
 public:
-    Receiver(ros::NodeHandle nm)
-    :  nm_(nm), hardware_threads(std::thread::hardware_concurrency()),
+    Receiver()
+    :  hardware_threads(std::thread::hardware_concurrency()),
        spinner(2), count(0), num_points(4), updatePose(false)
-    {       
+    {
        I3.setIdentity(3, 3);
     }
 
     ~Receiver()
-    {         
+    {
         // rotoTransThread.detach();
     }
 
@@ -91,47 +89,46 @@ private:
     void spawn()
     {
         if(spinner.canStart())
-            spinner.start(); 
-        running = true; 
-        
-        sub_markers = nm_.subscribe("/vicon/markers", 10, &Receiver::callback, this);   
+            spinner.start();
+        running = true;
 
-        while(!updatePose)
-        {
-            if(!ros::ok())
-            {
+        sub_markers = nm_.subscribe("/vicon/markers", 10, &Receiver::callback, this);
+
+        while(!updatePose) {
+            if(!ros::ok()) {
               return;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));     
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
 
-        //spawn the threads
+        // spawn the threads
         rotoTransThread = std::thread(&Receiver::processRotoTrans, this);
     }
 
     void unspawn()
     {
         spinner.stop();
-
-        if(rotoTransThread.joinable())
-            rotoTransThread.join();
+        // ROS_INFO("Got here");
 
         running = false;
+        if(rotoTransThread.joinable())
+            rotoTransThread.join();
+        // ROS_INFO("Got here again");
     }
 
     void callback(const vicon_bridge::MarkersConstPtr& markers_msg)
-    {   
-        //solve all vicon markers here
+    {
+        // solve all vicon markers here
         std::vector<geometry_msgs::Point> headMarkersVector;
         headMarkersVector.resize(4);
-        headMarkersVector[0] = markers_msg -> markers[0].translation;  //fore
-        headMarkersVector[1] = markers_msg -> markers[1].translation;   //left
-        headMarkersVector[2] = markers_msg -> markers[2].translation;   //chin
-        headMarkersVector[3] = markers_msg -> markers[3].translation;   //right
+        headMarkersVector[0] = markers_msg -> markers[0].translation;   // fore
+        headMarkersVector[1] = markers_msg -> markers[1].translation;   // left
+        headMarkersVector[2] = markers_msg -> markers[2].translation;   // chin
+        headMarkersVector[3] = markers_msg -> markers[3].translation;   // right
 
         std::lock_guard<std::mutex> lock(mutex);
         this->headMarkersVector = headMarkersVector;
-        updatePose          = true;   
+        updatePose          = true;
         ++count;
     }
 
@@ -139,7 +136,7 @@ private:
     {
         double mu_x, mu_y, mu_z = 0;
         for(auto i = 0; i < num_points; ++i)
-        {            
+        {
             mu_x += headMarkersVector[i].x;
             mu_y += headMarkersVector[i].y;
             mu_z += headMarkersVector[i].z;
@@ -159,127 +156,135 @@ private:
 
     void point_to_eigen(std::vector<geometry_msgs::Point>&& pt, std::vector<Vector3d>&& face_vec)
     {
-        face_vec[0] << pt[0].x, pt[0].y, pt[0].z; 
-        face_vec[1] << pt[1].x, pt[1].y, pt[1].z;  
-        face_vec[2] << pt[2].x, pt[2].y, pt[2].z;    
-        face_vec[3] << pt[3].x, pt[3].y, pt[3].z; 
+        face_vec[0] << pt[0].x, pt[0].y, pt[0].z;
+        face_vec[1] << pt[1].x, pt[1].y, pt[1].z;
+        face_vec[2] << pt[2].x, pt[2].y, pt[2].z;
+        face_vec[3] << pt[3].x, pt[3].y, pt[3].z;
 
-        //compute average of markers
+        // compute average of markers
         face_translation.x = 0.25*(pt[0].x, pt[1].x, pt[2].x, pt[3].x);
         face_translation.y = 0.25*(pt[0].y, pt[1].y, pt[2].y, pt[3].y);
         face_translation.z = 0.25*(pt[0].z, pt[1].z, pt[2].z, pt[3].z);
     }
 
 
-    //this closely follows pg 243 of the ICP paper by Besl and McKay
+    // this closely follows pg 243 of the ICP paper by Besl and McKay
     void processRotoTrans()
     {
         if(count == 1)         //store away zero pose of head
-        {   
+        {
             firstHeadMarkersVector.resize(num_points);
             first_face_vec.resize(num_points);
 
             std::lock_guard<std::mutex> lock(mutex);
-            firstHeadMarkersVector = this->headMarkersVector;            
+            firstHeadMarkersVector = this->headMarkersVector;
             remove_mean(std::move(firstHeadMarkersVector));
             //convert from geometry points to eigen
             point_to_eigen(std::move(firstHeadMarkersVector), std::move(first_face_vec));
         }
 
         std::vector<geometry_msgs::Point> headMarkersVector;
-        if(updatePose)
-        {   
-            headMarkersVector.resize(num_points);
-            std::lock_guard<std::mutex> lock(mutex);
-            headMarkersVector = this->headMarkersVector;
-            updatePose = false;
+
+        for(; running && ros::ok() ;)
+        {          
+            if(updatePose)
+            {
+                headMarkersVector.resize(num_points);
+                std::lock_guard<std::mutex> lock(mutex);
+                headMarkersVector = this->headMarkersVector;
+
+                //compute center of mass of model and measured point set
+                remove_mean(std::move(headMarkersVector));
+                //convert from geometry points to eigen
+                face_vec.resize(num_points);
+                point_to_eigen(std::move(headMarkersVector), std::move(face_vec));
+                //compute the cross covariance matrix of the points sets P and X
+                sigma_px.resize(3, 3);
+
+                sigma_px =  first_face_vec[0] * face_vec[0].transpose() +
+                            first_face_vec[1] * face_vec[1].transpose() +
+                            first_face_vec[2] * face_vec[2].transpose() +
+                            first_face_vec[3] * face_vec[3].transpose() ;
+                sigma_px /= num_points;
+
+                // ROS_INFO_STREAM("sigma_px: \n\n" << sigma_px);
+                // ROS_INFO_STREAM("sigma_px^T: \n\n" << sigma_px.transpose());
+
+                A_Mat.resize(3, 3);
+                // find the cyclic components of the skew symmetric matric A_{ij} = (sigma_{px} + sigma_{px}^T )_{ij}
+                A_Mat = sigma_px - sigma_px.transpose();
+
+                //collect cyclic components of skew symmetric matrix
+                Delta << A_Mat(1,2), A_Mat(2, 0), A_Mat(0, 1);
+
+                temp.resize(3,3);
+                temp = sigma_px + sigma_px.transpose() - (sigma_px.trace() * I3);
+
+                // Form the symmetric 4x4 Q matrix
+                Q(0, 0) =  sigma_px.trace();      Q(0, 1) = A_Mat(1,2);         Q(0, 2) = A_Mat(2, 0);   Q(0, 3) = A_Mat(0, 1);
+                Q(1, 0) =  A_Mat(1,2);            Q(1, 1) = temp(0, 0);         Q(1, 2) = temp(0, 1);    Q(1, 3) = temp(0, 2);
+                Q(2, 0) =  A_Mat(2,0);            Q(2, 1) = temp(1, 0);         Q(2, 2) = temp(1, 1);    Q(2, 3) = temp(1, 2);
+                Q(3, 0) =  A_Mat(0,1);            Q(3, 1) = temp(2, 0);         Q(3, 2) = temp(2, 1);    Q(3, 3) = temp(2, 2);
+
+                // ROS_INFO_STREAM("Q Matrix: " <<  Q);
+
+                //we now find the maximum eigen value of the matrix Q
+                EigenSolver<Matrix4d> eig(Q);
+
+                // Note that eigVal and eigVec are std::complex types. To access their
+                // real or imaginary parts, call real or imag
+                EigenSolver< Matrix4d >::EigenvalueType eigVals = eig.eigenvalues();
+                EigenSolver< Matrix4d >::EigenvectorsType eigVecs = eig.eigenvectors();
+
+                // std::cout << "vector types: " << eigVecs.col(0)  << typeid(eigVecs.col(0)).name() << "\n";
+                findQuaternion(std::move(eigVals), std::move(eigVecs));
+                updatePose = false;
+            }
         }
-
-        //compute center of mass of model and measured point set
-        remove_mean(std::move(headMarkersVector));
-        //convert from geometry points to eigen
-        face_vec.resize(num_points);
-        point_to_eigen(std::move(headMarkersVector), std::move(face_vec));
-        //compute the cross covariance matrix of the points sets P and X
-        sigma_px.resize(3, 3);
-
-        sigma_px =  first_face_vec[0] * face_vec[0].transpose() +
-                    first_face_vec[1] * face_vec[1].transpose() + 
-                    first_face_vec[2] * face_vec[2].transpose() + 
-                    first_face_vec[3] * face_vec[3].transpose() ;       
-        sigma_px /= num_points;
-
-        ROS_INFO_STREAM("sigma_px: \n\n" << sigma_px);
-        ROS_INFO_STREAM("sigma_px^T: \n\n" << sigma_px.transpose());
-
-        A_Mat.resize(3, 3);
-        //find the cyclic components of the skew symmetric matric A_{ij} = (sigma_{px} + sigma_{px}^T )_{ij}
-        A_Mat = sigma_px - sigma_px.transpose();
-
-        //collect cyclic components of skew symmetric matrix
-        Delta << A_Mat(1,2), A_Mat(2, 0), A_Mat(0, 1);
-
-        temp.resize(3,3);
-        temp = sigma_px + sigma_px.transpose() - (sigma_px.trace() * I3);
-
-        //Form the Q matrix
-        Q(0, 0) =  sigma_px.trace();      Q(0, 1) = A_Mat(1,2);         Q(0, 2) = A_Mat(2, 0);   Q(0, 3) = A_Mat(0, 1);
-        Q(1, 0) =  A_Mat(1,2);            Q(1, 1) = temp(0, 0);         Q(1, 2) = temp(0, 1);    Q(1, 3) = temp(0, 2);      
-        Q(2, 0) =  A_Mat(2,0);            Q(2, 1) = temp(1, 0);         Q(2, 2) = temp(1, 1);    Q(2, 3) = temp(1, 2);
-        Q(3, 0) =  A_Mat(0,1);            Q(3, 1) = temp(2, 0);         Q(3, 2) = temp(2, 1);    Q(3, 3) = temp(2, 2);
-
-        ROS_INFO_STREAM("Q Matrix: " <<  Q);
-
-        //we now find the maximum eigen value of the matrix Q
-        EigenSolver<Matrix4d> eig(Q);
-
-        // Note that eigVal and eigVec are std::complex types. To access their 
-        // real or imaginary parts, call real or imag
-        EigenSolver< Matrix4d >::EigenvalueType eigVals = eig.eigenvalues();
-        EigenSolver< Matrix4d >::EigenvectorsType eigVecs = eig.eigenvectors();
-
-        // std::cout << "vector types: " << eigVecs.col(0)  << typeid(eigVecs.col(0)).name() << "\n";
-
-        findQuaternion(std::move(eigVals), std::move(eigVecs));
     }
 
     void findQuaternion(EigenSolver< Matrix4d >::EigenvalueType&& eigVals, EigenSolver< Matrix4d >::EigenvectorsType && eigVecs)
     {
         //create a look-up table of eig vectors and values
         std::vector<double> valueVectors {eigVals[0].real(), eigVals[1].real(), eigVals[2].real(), eigVals[3].real()};
-    
+
         auto max = valueVectors[0];
-        int magicIdx;
-        for(auto i = 1; i < valueVectors.size(); ++i)        {
+        int magicIdx = 0;
+        for(int i = 0; i < valueVectors.size(); ++i)        {
             if(valueVectors[i] > max){
               max = valueVectors[i];
-              magicIdx = i;              
+              magicIdx = i;
             }
-
+            // ROS_INFO_STREAM("eigVals: " << valueVectors[i] << " | magicIdx:  " << magicIdx);
         }
-        // std::cout << "current max = " << max.real() << typeid(max).name() << "\n";
-        //find the eigen vector with the largest eigen value, This would be the optimal rotation quaternion 
-        auto optimalEigVec = eigVecs.col(magicIdx);  
-        
+        // find the eigen vector with the largest eigen value, This would be the optimal rotation quaternion
+        auto optimalEigVec = eigVecs.col(magicIdx);
+        // ROS_INFO_STREAM(" max eigVal = " << max << typeid(max).name() << "\tmagicIdx: " << magicIdx<< "\tmaxEigVec: " << optimalEigVec << "\n");
+        // ROS_INFO_STREAM("EigenVectors: " << eigVecs); // << type(eigVecs).name());
+        // compute optimal translation vector
+
         pose_info.translation.x = face_translation.x;
         pose_info.translation.y = face_translation.y;
         pose_info.translation.z = face_translation.z;
-
+        
         pose_info.rotation.x = optimalEigVec[1].real();
         pose_info.rotation.y = optimalEigVec[2].real();
         pose_info.rotation.z = optimalEigVec[3].real();
         pose_info.rotation.w = optimalEigVec[0].real();
-
+        
         tf::Quaternion quart(pose_info.rotation.x, \
                         pose_info.rotation.y, \
                         pose_info.rotation.z, \
                         pose_info.rotation.w);
-
+        
         tf::Matrix3x3 Rot(quart);
         Rot.getRPY(roll, pitch, yaw);
-
-        ROS_INFO_STREAM("pose_info: " << pose_info);
-        ROS_INFO("roll: %d | pitch: %d | yaw: %d ", roll, pitch, yaw);
+        //
+        // ROS_INFO_STREAM("pose_info: " << pose_info);
+        ROS_INFO("x: %.3f | y: %.3f | z: %.3f | roll: %.3f | pitch: %.3f | yaw: %.3f ", pose_info.translation.x, \
+                                                                            pose_info.translation.y, \
+                                                                            pose_info.translation.z, \
+                                                                            roll, pitch, yaw);
     }
 };
 
@@ -287,16 +292,14 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "vicon_icp_node");
 
-    ros::NodeHandle np;
+    if(!ros::ok())   
+      return EXIT_SUCCESS;
 
-    Receiver rcvr(np);
+    ROS_INFO_STREAM("Started node " << ros::this_node::getName().c_str());
 
+    Receiver rcvr;
     rcvr.run();
 
-    if(!ros::ok())
-    {
-      return 0;
-    }
-
     ros::shutdown();
+    return EXIT_SUCCESS;
 }
