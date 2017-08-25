207e66c298dfb283a63e6a2c352ff15411da8f72
lakehanne <patlekano@gmail.com>
2017-08-25 17:41:53 -0500

    processing spheros

--- icra/src/data/prepro.m
+++
@@ -1,2 +0,0 @@
-cd('~/catkin_ws/src/soft-neuro-adapt/icra/src/data')
-tx90 = load('tx90.xls')
--- icra/src/utils/data_loader.py
+++ icra/src/utils/data_loader.py
@@ -1,1 +1,12 @@
 import scipy.io as sio
+import time
+
+
+sphero_data = sio.loadmat("../data/sphero.mat")
+
+for k, v in sphero_data.items():
+    for k in sphero_data['exp']:
+        print(k, v)
+        time.sleep(2)
+# expt_I = sphero_data['expt']
+# print(expt_I)
